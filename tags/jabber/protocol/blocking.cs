b556398f1e280c21be5ef66466e952b59a4c8e52
Dmitry Chabanin <dmitry.chabanin@gmail.com>
2015-05-04 12:03:28 +0300

    Updated

---
+++ tags/jabber/protocol/blocking.cs
@@ -0,0 +1,193 @@
+﻿// Copyright © 2006 - 2012 Dieter Lunn
+// Modified 2012 Paul Freund ( freund.paul@lvl3.org )
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+//
+// You should have received a copy of the GNU Lesser General Public License along
+// with this library; if not, write to the Free Software Foundation, Inc., 59
+// Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+using System.Collections.Generic;
+using System.Xml.Linq;
+using XMPP.Registries;
+
+namespace XMPP.Tags.Jabber.Blocking
+{
+    public class Namespace
+    {
+        public const string XmlNamespace = "urn:xmpp:blocking";
+
+        public static readonly XName Block = XName.Get("block", XmlNamespace);
+        public static readonly XName Unblock = XName.Get("unblock", XmlNamespace);
+        public static readonly XName Blocklist = XName.Get("blocklist", XmlNamespace);
+        public static readonly XName Item = XName.Get("item", XmlNamespace);
+    }
+
+    [XmppTag(typeof(Namespace), typeof(Block))]
+    public class Block : Tag
+    {
+        public Block()
+            : base(Namespace.Block)
+        {
+        }
+
+        public Block(XElement other)
+            : base(other)
+        {
+        }
+
+        public IEnumerable<Item> ItemElements
+        {
+            get { return Elements<Item>(Namespace.Item); }
+        }
+    }
+
+    [XmppTag(typeof(Namespace), typeof(Unblock))]
+    public class Unblock : Tag
+    {
+        public Unblock()
+            : base(Namespace.Unblock)
+        {
+        }
+
+        public Unblock(XElement other)
+            : base(other)
+        {
+        }
+
+        public IEnumerable<Item> ItemElements
+        {
+            get { return Elements<Item>(Namespace.Item); }
+        }
+    }
+
+    [XmppTag(typeof(Namespace), typeof(Item))]
+    public class Item : Tag
+    {
+        public Item()
+            : base(Namespace.Item)
+        {
+        }
+
+        public Item(XElement other)
+            : base(other)
+        {
+        }
+
+        public string JidAttr
+        {
+            get { return (string)GetAttributeValue("jid"); }
+            set { InnerElement.SetAttributeValue("jid", value); }
+        }
+    }
+
+    [XmppTag(typeof(Namespace), typeof(Blocklist))]
+    public class Blocklist : Tag
+    {
+        public Blocklist()
+            : base(Namespace.Blocklist)
+        {
+        }
+
+        public Blocklist(XElement other)
+            : base(other)
+        {
+        }
+
+        public IEnumerable<Item> ItemElements
+        {
+            get { return Elements<Item>(Namespace.Item); }
+        }
+    }
+}
+
+/*
+<?xml version='1.0' encoding='UTF-8'?>
+<xs:schema
+    xmlns:xs='http://www.w3.org/2001/XMLSchema'
+    targetNamespace='urn:xmpp:blocking'
+    xmlns='urn:xmpp:blocking'
+    elementFormDefault='qualified'>
+
+  <xs:annotation>
+    <xs:documentation>
+      The protocol documented by this schema is defined in
+      XEP-0191: http://www.xmpp.org/extensions/xep-0191.html
+    </xs:documentation>
+  </xs:annotation>
+
+  <xs:element name='block'>
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element ref='item' minOccurs='1' maxOccurs='unbounded'/>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
+
+
+  <xs:element name='unblock'>
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element ref='item' minOccurs='0' maxOccurs='unbounded'/>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
+
+
+  <xs:element name='blocklist'>
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element ref='item' minOccurs='0' maxOccurs='unbounded'/>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
+
+  <xs:element name='item'>
+    <xs:complexType>
+      <xs:simpleContent>
+        <xs:extension base='empty'>
+          <xs:attribute name='jid' type='xs:string' use='required'/>
+        </xs:extension>
+      </xs:simpleContent>
+    </xs:complexType>
+  </xs:element>
+
+  <xs:simpleType name='empty'>
+    <xs:restriction base='xs:string'>
+      <xs:enumeration value=''/>
+    </xs:restriction>
+  </xs:simpleType>
+
+</xs:schema>
+
+<?xml version='1.0' encoding='UTF-8'?>
+<xs:schema
+    xmlns:xs='http://www.w3.org/2001/XMLSchema'
+    targetNamespace='urn:xmpp:blocking:errors'
+    xmlns='urn:xmpp:blocking:errors'
+    elementFormDefault='qualified'>
+
+  <xs:annotation>
+    <xs:documentation>
+      The protocol documented by this schema is defined in
+      XEP-0191: http://www.xmpp.org/extensions/xep-0191.html
+    </xs:documentation>
+  </xs:annotation>
+
+  <xs:element name='blocked' type='empty'/>
+
+  <xs:simpleType name='empty'>
+    <xs:restriction base='xs:string'>
+      <xs:enumeration value=''/>
+    </xs:restriction>
+  </xs:simpleType>
+
+</xs:schema>
+*/
